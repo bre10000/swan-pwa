10e873e1448f12894c96d98b7b4c67078f973318
bre10000 <burukaddis@gmail.com>
2022-05-26 00:08:36 +0300

    add

---
+++ swan-pwa/client/index.7cfcf5ca.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as t,u as o,g as a,o as n}from"./client.8b941b19.js";function r(s){const e=o.subscribe((s=>{!s.loggedIn&&s.fetched?a("/login"):s.loggedIn&&s.fetched&&a("/dashboard")}));return n(e),[]}class c extends s{constructor(s){super(),e(this,s,r,null,t,{})}}export{c as default};
