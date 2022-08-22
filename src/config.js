e66d0f5882a0694ecd8b24caf14a5b14050eef7d
bre10000 <burukaddis@gmail.com>
2022-08-22 20:30:34 +0300

    Update Config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const server_ip = 'http://localhost:1337'
 // const www_ip = 'https://swan-stock-portal.herokuapp.com'
 const www_ip = 'http://swan.swanstockportal.com'
 
+
 const api_url = (process.env.NODE_ENV === 'production'
 ? www_ip 
 : server_ip) + '/api';
